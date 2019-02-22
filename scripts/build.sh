d294d7aa0312a8fb91f59a8d8d616149a08676fb
Chris Xu <gzchrisxjh@gmail.com>
2019-02-22 00:53:28 -0500

    Fix ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 ./validate-style.sh
+
+echo "Building app..."
+yarn build --base-href https://follothru.github.io/follothru-frontend/
+
+cd ./dist/follothru-frontend
+cp ./index.html ./404.html
