f8d0f7428c4ba8ff0395dd628d5eebc9123b724c
Chris Xu <gzchrisxjh@gmail.com>
2019-02-22 01:20:34 -0500

    Fix ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,6 @@ echo "------"
 echo "Building & deploying app..."
 ng build --base-href https://follothru.github.io/follothru-frontend/
 
+ls
 cd ./dist/follothru-frontend
 cp ./index.html ./404.html
