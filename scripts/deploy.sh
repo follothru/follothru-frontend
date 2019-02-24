625c57df2c30b130462e544f3b570406b0a245ad
follothru <46720393+follothru@users.noreply.github.com>
2019-02-24 00:39:35 -0500

    Add slash

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "------"
 echo "Building & deploying app..."
-ng build --prod --base-href https://follothru.github.io/follothru-frontend
+ng build --prod --base-href https://follothru.github.io/follothru-frontend/
 
 ls
 cd ./dist/follothru-frontend
