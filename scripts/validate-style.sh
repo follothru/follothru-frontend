973844855fcc3995ce701d525407db67409e0e5f
Chris Xu <gzchrisxjh@gmail.com>
2019-03-14 18:29:25 -0400

    Fix ci

--- scripts/validate-style.sh
+++ scripts/validate-style.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "------"
-echo "Validating code formatting (yarn prettier)..."
+echo "Validating code formatting (npm run prettier)..."
 npm run prettier
 if [ $? -eq 0 ]; then
   echo "SUCCESS: All code has been formatted correctly."
@@ -9,7 +9,7 @@ else
   exit 1
 fi
 
-echo "Validing code pattern (npm eslint)..."
+echo "Validing code pattern (npm run eslint)..."
 npm run lint
 if [ $? -eq 0 ]; then
   echo "SUCCESS: eslint has paassed."
